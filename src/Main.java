7fac4f59bf808d52f0f49a4e50b2851005d25f01
agorohov <aleksandr.gorohov@gmail.com>
2024-03-19 16:44:15 +0300

    task complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        Author gerbertShildt = new Author("Герберт", "Шилдт");
+        Author bruceEkkel = new Author("Брюс", "Эккель");
+        Book JavaABeginnersGuide = new Book("Java. Руководство для начинающих", gerbertShildt, 2023);
+        Book javaPhilosophy = new Book("Философия Java", bruceEkkel, 1998);
+        javaPhilosophy.setPublicationYear(2024);
     }
 }
